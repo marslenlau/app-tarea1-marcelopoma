747383ad4c0fe944b8d07121f22f58a354b6e036
Marcelo Poma <marcelo.pc.dev@gmail.com>
2024-03-12 03:38:29 -0400

    ADD: desarrollo de tarea numero 1 MSM

---
+++ src/app/pages/inicio/inicio.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Componente } from 'src/app/interfaces/interfaces';
+import { DataService } from 'src/app/services/data.service';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.page.html',
+  styleUrls: ['./inicio.page.scss'],
+})
+export class InicioPage implements OnInit {
+  componentes?: Observable<Componente[]>;
+  constructor(
+    private dataService: DataService
+  ) { }
+
+  ngOnInit() {
+    this.componentes = this.dataService.getMenuOpts();
+  }
+
+}
